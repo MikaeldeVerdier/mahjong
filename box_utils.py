d47eacf47ddddfd126beb44afaaccd742065b03e
Mikael de Verdier <mikael.deverdier@gmail.com>
2024-04-06 01:07:26 +0200

    change to how variance is used

--- box_utils.py
+++ box_utils.py
@@ -134,16 +134,14 @@ def plot_ious(gts, boxes, img, labels=None, confidences=None, name="boxes.png", 
 	plt.close()
 
 
-def calculate_offset(gt, boxes, sq_variances=None):
+def calculate_offset(gt, boxes, sq_variances):
 	cx = (gt[0] - boxes[:, 0]) / boxes[:, 2]
 	cy = (gt[1] - boxes[:, 1]) / boxes[:, 3]
 	w = np.log(gt[2] / boxes[:, 2])
 	h = np.log(gt[3] / boxes[:, 3])
 
 	offset = np.moveaxis([cx, cy, w, h], 0, -1)
-
-	if sq_variances is not None:
-		offset /= np.sqrt(sq_variances)
+	offset /= sq_variances
 
 	return offset
 
