1991ab3442a2a993bfe929ec96273a673752c61c
Mikael de Verdier <mikael.deverdier@gmail.com>
2024-09-24 23:25:18 +0200

    VERSION 3.5: further training on Version 2 with new images with more tile faces

--- ssd/config.py
+++ ssd/config.py
@@ -1,14 +1,14 @@
 # Training configuration
 TRAINING_SPLIT = 1
 USE_AUGMENTATION = True
-LEARNING_RATE = {0: 1e-3, 123_000: 1e-4, 140_000: 1e-5}
+LEARNING_RATE = {0: 1e-3, 100_000: 1e-4, 150_000: 1e-5}
 MOMENTUM = 0.9
 ALPHA = 1
 L2_REG = 5e-4
 HARD_NEGATIVE_RATIO = 3
 VARIANCES = [0.1, 0.1, 0.2, 0.2]
 BATCH_SIZE = 16
-TRAINING_ITERATIONS = 150_000
+TRAINING_ITERATIONS = 500_000 - 294_720
 EPOCHS = 1
 VALIDATION_SPLIT = 0.2
 
@@ -16,6 +16,8 @@ VALIDATION_SPLIT = 0.2
 SAVE_FOLDER_PATH = "save_folder"
 SAVING_RATIO = 0.05
 
+# MODEL INFO:
+# Based on Version 2
+
 # DATASET INFO:
-# Synthetic data: 12k images (same dataset as Version 2, only clipped at 12k (in order of annotations file))
-# Real data: 12.291k images (only 11.976k used due to ignore_no_gt in preparation being True)
+# Synthetic data: 100k images (newly generated)
