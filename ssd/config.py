b2717f0c79f313350c01f07c50ffae49e72aba9b
Mikael de Verdier <mikael.deverdier@gmail.com>
2024-10-03 16:56:27 +0200

    update to config to inform of dispruptive version 3.5 training

--- ssd/config.py
+++ ssd/config.py
@@ -1,13 +1,13 @@
 # Training configuration
 TRAINING_SPLIT = 1
 USE_AUGMENTATION = True
-LEARNING_RATE = {0: 1e-3, 100_000: 1e-4, 150_000: 1e-5}
+LEARNING_RATE = {0: 1e-3, 100_000: 1e-4, 110_000: 1e-3, 150_000: 1e-5}  # Just an estimation
 MOMENTUM = 0.9
 ALPHA = 1
 L2_REG = 5e-4
 HARD_NEGATIVE_RATIO = 3
 VARIANCES = [0.1, 0.1, 0.2, 0.2]
-BATCH_SIZE = 16
+BATCH_SIZE = 16  # Except around last 8k iterations, where it was 8
 TRAINING_ITERATIONS = 500_000 - 294_720
 EPOCHS = 1
 VALIDATION_SPLIT = 0.2
